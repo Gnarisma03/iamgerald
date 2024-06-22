295c11060409c2f146e5019f1be7316f80267489
Gnarisma03 <156625005+Gnarisma03@users.noreply.github.com>
2024-06-22 19:09:51 +0800

    Update script.js

--- MyPortfolio/functions/script.js
+++ MyPortfolio/functions/script.js
@@ -1,3 +1,5 @@
+
+
 let sections = document.querySelectorAll('section');
 let navLinks = document.querySelectorAll(' .header-main nav ul li a');
 
@@ -18,4 +20,28 @@ window.onscroll = () => {
 
         };
     });
+};
+
+
+const hamburger = document.querySelector(".hamburger");
+const navMenu = document.querySelector(".header-menu-list");
+
+hamburger.addEventListener("click", () => {
+  hamburger.classList.toggle("active");
+  navMenu.classList.toggle("active");
+})
+
+document.querySelectorAll(".header-menu-list li").forEach(n => n.addEventListener("click", () => {
+  hamburger.classList.remove("active");
+  navMenu.classList.remove("active");
+}));
+
+
+var navBar = document.getElementById("header-main");
+window.onscroll = function() {
+  if (window.scrollY > 22) {
+    navBar.classList.add("scrolled");
+  } else {
+    navBar.classList.remove("scrolled");
+  }
 };
